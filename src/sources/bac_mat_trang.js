7367a84f7afd208319811780348fe0b3c16d2301
Dat Ngo <dat.ngotan@axonactive.com>
2026-03-17 14:34:55 +0700

    fix: node

--- src/sources/bac_mat_trang.js
+++ src/sources/bac_mat_trang.js
@@ -18,7 +18,7 @@ function parse(html, needle, unit) {
 }
 
 const SHARED = {
-  storeName: "Bac Mat Trang",
+  storeName: "Bạc Mặt Trăng",
   url: "https://bacmattrang.com/bac_mat_trang/default/index",
   webUrl: "https://bacmattrang.com/bac_mat_trang/default/index",
   location: "TP.HCM",
@@ -28,14 +28,14 @@ export const BAC_MAT_TRANG_SOURCES = [
   {
     ...SHARED,
     id: "bac_mat_trang_bac_khoi_1_luong",
-    name: "Bac Mat Trang - Bac Khoi 1 Luong",
+    name: "Bạc Mặt Trăng - Bạc Khối",
     unit: "luong",
     parse: (html) => parse(html, "1 Lượng", "luong"),
   },
   {
     ...SHARED,
     id: "bac_mat_trang_bac_khoi_1_kg",
-    name: "Bac Mat Trang - Bac Khoi 1 Kg",
+    name: "Bạc Mặt Trăng - Bạc Khối",
     unit: "kg",
     parse: (html) => parse(html, "1 Kg", "kg"),
   },
